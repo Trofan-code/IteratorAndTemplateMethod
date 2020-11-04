b799c0da95cedc2e8db9e44a6faad981f7c9f75a
Yevgeniia Trofan <mrs.trofan@gmail.com>
2020-11-04 18:06:18 +0100

    2 commit for dev

--- app/src/main/java/com/example/taskfox3/fragment/BlankFragment.java
+++ app/src/main/java/com/example/taskfox3/fragment/BlankFragment.java
@@ -1,17 +1,19 @@
 package com.example.taskfox3.fragment;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.taskfox3.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BlankFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
 public class BlankFragment extends Fragment {
 
@@ -23,6 +25,10 @@ public class BlankFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
 
     /**
      * Use this factory method to create a new instance of
@@ -40,10 +46,6 @@ public class BlankFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public BlankFragment() {
-        // Required empty public constructor
     }
 
     @Override
